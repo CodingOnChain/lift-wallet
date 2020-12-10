201ca5595a5170da1327b664ba64c82686c65b7e
Kyle Johns <kjohns1337@gmail.com>
2020-12-09 21:52:27 -0500

    stubbing components

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@
         v-bind:loading="bootingCnode" 
         v-bind:syncing="syncingCnode"
         v-bind:progress="syncingProgress" />
+      <MainView v-show="activeCnode" />
     </v-main>
   </v-app>
 </template>
@@ -26,12 +27,14 @@
 <script>
 const { ipcRenderer } = require('electron')
 import LoadingCnode from './components/LoadingCnode'
+import MainView from './components/MainView'
 
 export default {
   name: 'App',
 
   components: {
-    LoadingCnode
+    LoadingCnode,
+    MainView
   },
 
   created(){
@@ -60,6 +63,11 @@ export default {
           this.syncingProgress = args.network.sync_progress.progress.quantity;
           console.log(`${args.network.sync_progress.progress.quantity}%`)
           this.getSyncInfo = setTimeout(() => {  ipcRenderer.send('req:get-network'); }, 10000);
+        }else {
+          console.log('synced!')
+          this.bootingCnode = false;
+          this.syncingCnode = false;
+          this.activeCnode = true;
         }
       }else {
           this.getSyncInfo = setTimeout(() => {  ipcRenderer.send('req:get-network'); }, 10000);
