b5f365ef4a39c9e0691aa7f61d9a2ab6ff44a129
Kyle Johns <kjohns1337@gmail.com>
2020-12-26 21:21:23 -0500

    base64 technique

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+  //https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/configuration.html#changing-the-output-directory
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
@@ -11,6 +12,12 @@ module.exports = {
         "mac": {
           "extraFiles": [
             "cardano/darwin/**/*",
+            "cardano/configs/**/*"
+          ]
+        },
+        "win": {
+          "extraFiles": [
+            "cardano/win32/**/*",
             "cardano/configs/**/*"
           ]
         }
