0511cfa1515476278bf740272c233d1f99607e91
Kyle Johns <kjohns1337@gmail.com>
2021-02-10 16:29:17 -0500

    hex to ascii function for token names

--- src/core/common.js
+++ src/core/common.js
@@ -40,3 +40,13 @@ export function decrypt(prvFile, pubFile, password) {
         throw exception.message;
     }
 }
+
+export function hex_to_ascii(str1)
+{
+   var hex  = str1.toString();
+   var str = '';
+   for (var n = 0; n < hex.length; n += 2) {
+       str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+   }
+   return str;
+}
