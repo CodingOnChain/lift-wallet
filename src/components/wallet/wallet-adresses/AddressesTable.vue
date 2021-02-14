540ef14523bfefa3ea33f99061e445bd3c921f66
Lisandro Iraguen <lisandro.iraguen@gmail.com>
2021-02-14 12:12:25 -0300

    indentation adjust

--- src/components/wallet/wallet-adresses/AddressesTable.vue
+++ src/components/wallet/wallet-adresses/AddressesTable.vue
@@ -2,42 +2,39 @@
   <div>
     <v-data-table
       :headers="headers"
-      :items="tableItems" 
+      :items="tableItems"
       disable-pagination
-      hide-default-footer     
-    >      
+      hide-default-footer
+    >
     </v-data-table>
   </div>
 </template>
 <script>
-  export default {
-    data () {
-      return {    
-      };
-    },
-    props: {
+export default {
+  data() {
+    return {};
+  },
+  props: {
     tableItems: Array,
+  },
+  computed: {
+    headers() {
+      return [
+        {
+          text: "Imdex",
+          align: "start",
+          sortable: false,
+          value: "index",
+        },
+        {
+          text: "Adress",
+          align: "end",
+          sortable: true,
+          value: "address",
+        },
+      ];
     },
-    computed: {
-      headers () {
-        return [
-          {
-            text: 'Imdex',
-            align: 'start',
-            sortable: false,
-            value: 'index',
-          },
-          {
-            text: 'Adress',
-            align: 'end',
-            sortable: true,
-            value: 'address',
-          }         
-        ];
-      },
-    },
-    methods: {
-      
-    },
-  };
+  },
+  methods: {},
+};
 </script>
